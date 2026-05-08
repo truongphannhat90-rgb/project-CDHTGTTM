475e62da18c0aa71ef74f23e55546324dabb7a15
Nhat Truong <truongphannhat90@gmail.com>
2026-05-08 18:10:42 +0700

    update lane function

--- Module/lane.py
+++ Module/lane.py
@@ -1,46 +1,26 @@
+import cv2
 import numpy as np
 
-class CentroidTracker:
+def detect_lanes(frame):
+    height, width = frame.shape[:2]
 
-    def __init__(self, max_distance=70):
-        self.nextObjectID = 0
-        self.boxes = {} # Lưu {id: [x1, y1, x2, y2, cls]}
-        self.max_distance = max_distance
+    # THIẾT LẬP RANH GIỚI CỐ ĐỊNH (FIXED BOUNDARY)
+    
+    # Điểm phía xa (Top)
+    x1_fixed = int(width * 0.61) 
+    y1_fixed = int(height * 0.45)
+    
+    # Điểm phía gần camera (Bottom)
+    x2_fixed = int(width * 0.65)
+    y2_fixed = height
 
-    def update(self, detections):
-        new_boxes = {}
+    # Lưu vào danh sách theo định dạng cũ để không làm lỗi các hàm khác
+    lane_lines = [np.array([[x1_fixed, y1_fixed, x2_fixed, y2_fixed]])]
 
-        for det in detections:
-            # Lấy đủ 5 tham số bao gồm cả Class ID (cls)
-            x1, y1, x2, y2, cls = det
+    #VẼ RANH GIỚI ĐỂ QUAN SÁT 
+    # cv2.line(frame, (x1_fixed, y1_fixed), (x2_fixed, y2_fixed), (255, 0, 255), 3)
+    # Hiển thị nhãn ranh giới
+    cv2.putText(frame, "RANH GIOI CHUAN", (x1_fixed - 50, y1_fixed - 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
 
-            cx = int((x1 + x2) / 2)
-            cy = int((y1 + y2) / 2)
-
-            matched_id = None
-            min_distance = float("inf")
-
-            for obj_id, old_data in self.boxes.items():
-                # Lấy dữ liệu cũ (old_data bây giờ có 5 phần tử)
-                ox1, oy1, ox2, oy2, old_cls = old_data[:5]
-
-                ocx = int((ox1 + ox2) / 2)
-                ocy = int((oy1 + oy2) / 2)
-
-                distance = np.sqrt((cx - ocx)**2 + (cy - ocy)**2)
-
-                # CẢI TIẾN: Chỉ match nếu cùng loại xe (cls == old_cls)
-                if cls == old_cls and distance < self.max_distance and distance < min_distance:
-                    matched_id = obj_id
-                    min_distance = distance
-
-            if matched_id is not None:
-                # Cập nhật xe cũ, giữ nguyên ID và lưu cả cls mới
-                new_boxes[matched_id] = [x1, y1, x2, y2, cls]
-            else:
-                # Tạo xe mới và lưu kèm cls
-                new_boxes[self.nextObjectID] = [x1, y1, x2, y2, cls]
-                self.nextObjectID += 1
-
-        # Cập nhật danh sách boxes chính
-        self.boxes = new_boxes
+    return lane_lines, frame
