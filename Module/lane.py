c45f719425d894a29bb1edff2237bcf0fb91d31d
thuanng1305 <thuan135206@gmail.com>
2026-05-13 16:56:54 +0700

    Update lane.py

--- Module/lane.py
+++ Module/lane.py
@@ -1,31 +1,26 @@
-from shapely.geometry import Point, Polygon
+import cv2
+import numpy as np
 
-class LaneChecker:
+def detect_lanes(frame):
+    height, width = frame.shape[:2]
 
-    def __init__(self, moto_polygon, car_polygon):
+    # THIẾT LẬP RANH GIỚI CỐ ĐỊNH (FIXED BOUNDARY)
+    
+    # Điểm phía xa (Top)
+    x1_fixed = int(width * 0.61) 
+    y1_fixed = int(height * 0.45)
+    
+    # Điểm phía gần camera (Bottom)
+    x2_fixed = int(width * 0.65)
+    y2_fixed = height
 
-        # Tạo polygon cho từng làn
-        self.moto_lane = Polygon(moto_polygon)
-        self.car_lane = Polygon(car_polygon)
+    # Lưu vào danh sách theo định dạng cũ để không làm lỗi các hàm khác
+    lane_lines = [np.array([[x1_fixed, y1_fixed, x2_fixed, y2_fixed]])]
 
-        # Buffer giúp tránh lỗi point nằm đúng mép lane
-        self.buffer_size = 5
+    #VẼ RANH GIỚI ĐỂ QUAN SÁT 
+    # cv2.line(frame, (x1_fixed, y1_fixed), (x2_fixed, y2_fixed), (255, 0, 255), 3)
+    # Hiển thị nhãn ranh giới
+    cv2.putText(frame, "RANH GIOI CHUAN", (x1_fixed - 50, y1_fixed - 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
 
-    def check_violation(self, bbox, class_id, moto_classes, car_classes):
-        x1, y1, x2, y2 = map(float, bbox)
-        cx = (x1 + x2) / 2
-        cy = y2
-        point = Point(cx, cy)
-
-        is_moto = class_id in moto_classes
-        is_car = class_id in car_classes
-        
-        in_moto_lane = (self.moto_lane.buffer(self.buffer_size).contains(point))
-        in_car_lane = (self.car_lane.buffer(self.buffer_size).contains(point))
-
-        if is_moto and in_car_lane:
-            return (True, "moto_in_car_lane")
-        elif is_car and in_moto_lane:
-            return (True, "car_in_moto_lane")
-        else:
-            return (False,"ok")
+    return lane_lines, frame
