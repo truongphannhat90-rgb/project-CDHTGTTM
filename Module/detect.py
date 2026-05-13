c7a5f1d7abaa05cf80ab80930297505ed5cccb3d
phan nhat truong <truongphannhat90@gmail.com>
2026-05-13 14:27:43 +0700

    Update detect.py

--- Module/detect.py
+++ Module/detect.py
@@ -22,22 +22,7 @@ class VehicleDetector:
                 x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
                 cls = int(box.cls[0])
                 
-                # --- LOGIC LỌC THÔNG MINH (FIX TRIỆT ĐỂ NHẬN DIỆN SAI) ---
-                w_box = x2 - x1
-                h_box = y2 - y1
-                area_ratio = (w_box * h_box) / (width * height) # Tỉ lệ diện tích xe/khung hình
-                aspect_ratio = w_box / (h_box + 0.001)         # Tỉ lệ ngang/cao
-
-                # 1. FIX Ô TÔ NHẦM THÀNH XE MÁY (Trường hợp ID 24 của bạn)
-                # Nếu AI bảo xe máy (3) nhưng diện tích > 3% khung hình HOẶC dáng xe quá rộng (ngang/cao > 0.8)
-                if cls == 3:
-                    if area_ratio > 0.03 or aspect_ratio > 0.8:
-                        cls = 2 # Ép về Car (Ô tô)
-
-                # 2. FIX XE MÁY NHẦM THÀNH Ô TÔ (Xe máy ở xa)
-                # Nếu AI bảo ô tô (2) nhưng dáng xe cao gầy (ngang/cao < 0.6)
-                if cls == 2 and aspect_ratio < 0.6:
-                    cls = 3 # Ép về Motorcycle (Xe máy)
+            
                 
                 detections.append([x1, y1, x2, y2, cls])
 
